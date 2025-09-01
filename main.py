df8f90ac54bbfb534365a32c85939cd187b9bb5e
siddhant754962 <123658041+siddhant754962@users.noreply.github.com>
2025-09-01 18:32:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Step 1: Load your trained model
-with open("\insurance_model.pkl", "rb") as file:
+with open("insurance_model.pkl", "rb") as file:
     model = pickle.load(file)
 
 st.title("Medical Insurance Cost Prediction App 💰🏥")
